c1161c7578b19defd0c95e7ebb1d7b63da7fb91e
Ke <ke@bob-2.local>
2022-09-11 12:53:19 -0500

    last batch

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function createNewElement() {
 	elem.innerHTML = `<div class="rounded overflow-hidden box-img" style="height: 100%; background-color: ${rngColor()};">
                             <img src="/artworks/${rngInteger(
 																													1,
-																													104
+																													115
 																												)}.PNG">
                           </div>`;
 	return elem;
